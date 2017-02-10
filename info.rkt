aa6171ae5b835d0af9c82b152f7dfac2774ae95c
Leandro Facchinetti <me@leafac.com>
2017-02-10 10:46:47 -0500

    Add missing dependency

--- info.rkt
+++ info.rkt
@@ -2,7 +2,7 @@
 
 (define collection "pollen-component")
 (define version "0.0.1")
-(define deps '("base" "pollen"))
+(define deps '("base" "pollen" "sugar"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("documentation/pollen-component.scrbl" ())))
 (define pkg-desc "Component-based development for Pollen")
